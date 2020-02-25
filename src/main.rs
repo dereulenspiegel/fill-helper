8256f4dcf3df771be57bd54c44c2a9734a740f40
Till Klocke <till.klocke@gmail.com>
2020-02-25 22:22:19 +0100

    Initial code. Builds and should read the rotary encoder

---
+++ src/main.rs
@@ -0,0 +1,109 @@
+#![deny(unsafe_code)]
+#![allow(clippy::missing_safety_doc)]
+#![deny(warnings)]
+#![no_main]
+#![no_std]
+
+use cortex_m_semihosting::{hprintln};
+use panic_semihosting as _;
+use rtfm::cyccnt::{U32Ext as _};
+//use stm32f1xx_hal::gpio::gpioc::*;
+use stm32f1xx_hal::gpio::gpiob::*;
+use stm32f1xx_hal::gpio::*;
+use stm32f1xx_hal::prelude::*;
+
+use rotary_encoder_hal::{Direction, Rotary};
+
+const ROTARY_ENCODER_PERIOD: u32 = 720_000;
+const FLOW_COUNTER_PERIOD: u32 = 500_000;
+
+enum ContainerSizes {
+    Bottle330ML,
+    Bottle500ML,
+    Bottle1L,
+    Bottle2L,
+    Keg5L,
+    Keg9_45L,
+    Keg18L,
+}
+
+// RTFM main declaration
+#[rtfm::app(device = stm32f1xx_hal::pac, peripherals = true, monotonic = rtfm::cyccnt::CYCCNT)]
+const APP: () = {
+    struct Resources {
+        rotary_encoder: Rotary<PB10<Input<PullUp>>,PB11<Input<PullUp>>>,
+    }
+
+    #[init(schedule = [scan_rotary_encoder, scan_flow_counter])]
+    fn init(mut cx: init::Context) -> init::LateResources{
+
+        cx.core.DCB.enable_trace();
+
+        // semantically, the monotonic timer is frozen at time "zero" during `init`
+        // NOTE do *not* call `Instant::now` in this context; it will return a nonsense value
+        let now = cx.start; // the start time of the system
+
+        // Device specific peripherals
+        let device: stm32f1xx_hal::pac::Peripherals = cx.device;
+
+        let mut rcc = device.RCC.constrain();
+        let mut _afio = device.AFIO.constrain(&mut rcc.apb2);
+        let mut flash = device.FLASH.constrain();
+        let _clocks = rcc
+            .cfgr
+            .use_hse(8.mhz())
+            .sysclk(72.mhz())
+            .pclk1(36.mhz())
+            .freeze(&mut flash.acr);
+
+        let mut portb = device.GPIOB.split(&mut rcc.apb2);
+        let mut portc = device.GPIOC.split(&mut rcc.apb2);
+
+        let mut _led_pin = portc.pc13.into_push_pull_output(&mut portc.crh);
+        let mut _relay_pin = portb.pb1.into_push_pull_output(&mut portb.crl);
+
+        let pin_a = portb
+            .pb10
+            .into_pull_up_input(&mut portb.crh);
+        let pin_b = portb
+            .pb11
+            .into_pull_up_input(&mut portb.crh);
+
+        let enc = Rotary::new(pin_a, pin_b);
+
+        hprintln!("init").unwrap();
+
+        cx.schedule.scan_rotary_encoder(now + ROTARY_ENCODER_PERIOD.cycles()).unwrap();
+        cx.schedule.scan_flow_counter(now + FLOW_COUNTER_PERIOD.cycles()).unwrap();
+
+        init::LateResources{
+            rotary_encoder: enc,
+        }
+    }
+
+    #[task(schedule = [scan_rotary_encoder], resources = [rotary_encoder])]
+    fn scan_rotary_encoder(cx: scan_rotary_encoder::Context) {
+
+        match cx.resources.rotary_encoder.update().unwrap() {
+            Direction::Clockwise => {
+
+            }
+            Direction::CounterClockwise => {
+
+            }
+            Direction::None => {}
+        }
+
+        cx.schedule.scan_rotary_encoder(cx.scheduled + ROTARY_ENCODER_PERIOD.cycles()).unwrap()
+    }
+
+    #[task(schedule = [scan_flow_counter])]
+    fn scan_flow_counter(cx: scan_flow_counter::Context) {
+
+        cx.schedule.scan_flow_counter(cx.scheduled + FLOW_COUNTER_PERIOD.cycles()).unwrap()
+    }
+
+    extern "C" {
+        fn USART1();
+    }
+};
